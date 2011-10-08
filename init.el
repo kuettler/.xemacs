94e76454d84f8f8c8e0b5408f35188111c60535c
Ulrich Kuettler <ulrich.kuettler@theppro.com>
2011-10-08 11:49:25 +0200

    frame size and position thanks to http://stackoverflow.com/questions/92971/how-do-i-set-the-size-of-emacs-window

--- init.el
+++ init.el
@@ -285,3 +285,28 @@ With arg, to it arg times."
 (require 'un-define)
 (set-coding-priority-list '(utf-8))
 (set-coding-category-system 'utf-8 'utf-8)
+
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+	  (progn
+		;; use 120 char wide window for largeish displays and smaller 80
+		;; column windows for smaller displays pick whatever numbers make
+		;; sense for you
+		(if (> (x-display-pixel-width) 1600)
+			(add-to-list 'default-frame-alist (cons 'width 120))
+		  (add-to-list 'default-frame-alist (cons 'width 90)))
+		;; for the height, subtract a couple hundred pixels from the screen
+		;; height (for panels, menubars and whatnot), then divide by the
+		;; height of a char to get the height we want
+		(add-to-list 'default-frame-alist 
+					 (cons 'height (/ (- (x-display-pixel-height) 70)
+									  ;(frame-char-height)
+									  14
+									  )))
+		(add-to-list 'default-frame-alist 
+					 (cons 'top 10))
+		(setq initial-frame-alist '((left . 0)))
+		)))
+
+(set-frame-size-according-to-resolution)
